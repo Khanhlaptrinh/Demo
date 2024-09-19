e73a94eb7ea6697213f68fc8708940643ac44107
Khanhlaptrinh <buiduykhanh0162003@gmail.com>
2024-09-19 16:48:42 +0700

    demo

---
+++ ss6/demo2.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    //bai 1:
+    //alert("Hello World!");
+    //bai 2:
+    //document.getElementById("messageDiv").innerHTML = "Hello World !";
+    //bai 3:
+    //document.write("Hello World !");
+    //bai 4:
+    console.log("Hello World !");
+}
