d4bd63c3c0b7098ee41ef66acb3146e0f7cb44cb
Khanhlaptrinh <buiduykhanh0162003@gmail.com>
2024-09-22 12:36:07 +0700

    ThucHanh5

--- ss7/ThucHanh5.js
+++ ss7/ThucHanh5.js
@@ -1,33 +1,68 @@
-function calculate(operation) {
+// function calculate(operation) {
+//     let num1 = parseFloat(document.getElementById("num1").value);
+//     let num2 = parseFloat(document.getElementById("num2").value);
+//     let result;
+//
+//     document.getElementById('btn-add').addEventListener('click', () => calculate('add'));
+//     document.getElementById('btn sub').addEventListener('click', () => calculate('sub'));
+//     document.getElementById('btn mul').addEventListener('click', () => calculate('mul'));
+//     document.getElementById('btn div').addEventListener('click', () => calculate('div'));
+//
+//     switch (operation) {
+//         case 'btn-add':
+//             result = num1 + num2;
+//             break;
+//         case 'btn-sub':
+//             result = num1 - num2;
+//             break;
+//         case 'btn-mul':
+//             result = num1 * num2;
+//             break;
+//         case 'btn-div':
+//             if (num2 === 0) {
+//                 document.getElementById('resultDivison').innerHTML = "0";
+//             }else {
+//                 result = num1 / num2;
+//             }
+//             break;
+//         default:
+//             break;
+//     }
+//     document.getElementById('resultDivison').innerText = `Result: ${result}`;
+//
+// }
+
+//Cach khac:
+function add() {
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
     let result;
-
-    document.getElementById('btn add').addEventListener('click', () => calculate('btn add'));
-    // document.querySelectorAll('btn sub').addEventListener('click', () => calculate('sub'));
-    // document.querySelectorAll('btn mul').addEventListener('click', () => calculate('mul'));
-    // document.querySelectorAll('btn div').addEventListener('click', () => calculate('div'));
-
-    switch (operation) {
-        case 'btn add':
-            result = num1 + num2;
-            break;
-        case 'btn sub':
-            result = num1 - num2;
-            break;
-        case 'btn mul':
-            result = num1 * num2;
-            break;
-        case 'btn div':
-            if (num2 === 0) {
-                document.getElementById('resultDivison').innertHTML = "0";
-            }else {
-                result = num1 / num2;
-            }
-            break;
-        default:
-            break;
-    }
-    document.getElementById('resultDivison').innerText = `Result: ${result}`;
-
+    result = num1 + num2;
+    document.getElementById('result').innerText = `Result: ${result}`;
 }
+function sub() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result;
+    result = num1 - num2;
+    document.getElementById('result').innerText = `Result: ${result}`;
+}
+function mul() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result;
+    result = num1 * num2;
+    document.getElementById('result').innerText = `Result: ${result}`;
+}
+function div() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result;
+    if (num2 === 0) {
+        document.getElementById('result').innerHTML = "0";
+    } else {
+        result = num1 / num2;
+    }
+    document.getElementById('result').innerText = `Result: ${result}`;
+}
+
